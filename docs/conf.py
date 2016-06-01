ee12caf16ca1a050e727ea3fbadfbc5dff0021c1
Qu Dong <qudong452@gmail.com>
2016-06-01 14:27:04 +0800

    remove dir
    Signed-off-by: Qu Dong <qudong452@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -16,15 +16,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../src'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../src'))
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.4.2'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
